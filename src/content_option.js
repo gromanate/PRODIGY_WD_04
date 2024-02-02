463e2c97e8afd0dc51d37c7d6989a30e7e00c327
Roman Ghimire <141548867+gromanate@users.noreply.github.com>
2024-02-02 12:39:21 +0545

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,186 @@
+const logotext = "ROMAN";
+const meta = {
+  title: "Roman Ghimire",
+  description:
+    "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+  title: "I’m Roman Ghimire",
+  animated: {
+    first: "I am a CSIT student",
+    second: "I am learning Full-Stack Development",
+    third: "I am a content-creator.",
+  },
+  description: "This is a demo webiste for practising react.",
+  your_img_url: "../profile.jpg",
+};
+
+const dataabout = {
+  title: "abit about my self",
+  aboutme:
+    "I am a CSIT student enrolled in MECHI MULTIPLE CAMPUS, Tribhuwan University. I come from Eastern Nepal, Jhapa.",
+};
+const worktimeline = [
+  {
+    jobtitle: "Monitor of Class",
+    where: "Newton's Education Academy",
+    date: "2014",
+  },
+  {
+    jobtitle: "Voice of week",
+    where: "Tiktok",
+    date: "2021",
+  },
+  {
+    jobtitle: "Creator of week",
+    where: "ALquds",
+    date: "2022",
+  },
+];
+
+const skills = [
+  {
+    name: "Python",
+    value: 35,
+  },
+  {
+    name: "Djano",
+    value: 20,
+  },
+  {
+    name: "Javascript",
+    value: 60,
+  },
+  {
+    name: "React",
+    value: 60,
+  },
+  {
+    name: "Jquery",
+    value: 50,
+  },
+];
+
+const services = [
+  {
+    title: "UI & UX Design",
+    description: "I have a deep interest in learning UI/UX Design",
+  },
+  {
+    title: "Front-End",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+  {
+    title: "Django Python",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/800/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/550/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "rougavko@gmail.com",
+  YOUR_FONE: "(977)9817996680",
+  description: "Contact me for any collaborations",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com/gromanate",
+  facebook: "https://facebook.com/roman.ghimire.7902",
+  linkedin: "https://linkedin.com/roman",
+  twitter: "https://twitter.com",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
